fe81960a37a874f7105fb8911d00f0f8ff009957
alzamer2 <alzamer2@gmail.com>
2017-05-16 10:32:37 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,2 @@
+import requests
+requests.get('https://api.crunchyroll.com/start_session.0.json?device_id=123&country_code=US&device_type=com.crunchyroll.iphone&access_token=QWjz212GspMHH9h&version=2313.8&locale=enUS').text
